7d19ba9f8105ad8e85e6d56b0de883c136666a24
dkstm95 <dkatm95@naver.com>
2019-04-01 14:38:19 +0900

    Initial Commit

---
+++ Checklist1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist1
+//
+//  Created by 안승일 on 01/04/2019.
+//  Copyright © 2019 Seung-Il Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
